9e6edac47dacae2b9039ff2ed237731298dfdade
Djin <32814285+DjinSenseii@users.noreply.github.com>
2022-02-24 20:33:02 +0100

    Toolbar tests

--- TestNavBar/app/src/main/java/com/example/testnavbar/MainActivity.java
+++ TestNavBar/app/src/main/java/com/example/testnavbar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.testnavbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
