8508dbe5dc23de6d6d1a29e5a36d7a38a8b12297
phamducminhpro1 <47885726+phamducminhpro1@users.noreply.github.com>
2022-03-01 15:52:10 +0100

    Added job description

--- app/src/main/java/com/example/appdev/MyAdapter.java
+++ app/src/main/java/com/example/appdev/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.appdev;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
@@ -29,10 +31,23 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder,int position) {
+        position = holder.getAdapterPosition();
         holder.myText1.setText(data1[position]);
         holder.myText2.setText(data2[position]);
         holder.myImage.setImageResource(images[position]);
+
+        int finalPosition = position;
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, JobDescriptionActivity.class);
+                intent.putExtra("data1", data1[finalPosition]);
+                intent.putExtra("data2", data2[finalPosition]);
+                intent.putExtra("myImage", images[finalPosition]);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -43,11 +58,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView myText1, myText2;
         ImageView myImage;
+        ConstraintLayout mainLayout;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             myText1 = itemView.findViewById(R.id.myTextView1);
             myText2 = itemView.findViewById(R.id.myTextView2);
             myImage = itemView.findViewById(R.id.myImageView);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
         }
     }
 }
