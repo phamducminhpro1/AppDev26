818484422054c6cee854d3d00fd586311d679d31
Kay Verbruggen <k.verbruggen@student.tue.nl>
2022-03-18 00:37:33 +0100

    list your own posts

--- app/src/main/java/com/example/appdev/R_postsFragment.java
+++ app/src/main/java/com/example/appdev/R_postsFragment.java
@@ -3,18 +3,35 @@ package com.example.appdev;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class R_postsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    PostListAdapter postListAdapter;
+    private ArrayList<Job> jobList = new ArrayList<>();
 
     public R_postsFragment() {
         // Required empty public constructor
-
     }
 
     @Override
@@ -22,6 +39,10 @@ public class R_postsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_r_posts, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        readPosts();
 
         Button postButton = view.findViewById(R.id.buttonPost);
         postButton.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +53,32 @@ public class R_postsFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void readPosts() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Jobs");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                jobList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Job job = snapshot.getValue(Job.class);
+
+                    if (job.posterId.equals(firebaseUser.getUid())) {
+                        jobList.add(job);
+                    }
+                }
+
+                postListAdapter = new PostListAdapter(getContext(), jobList);
+                recyclerView.setAdapter(postListAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
