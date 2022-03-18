18419340ebfdbe60182d487a2547ef73e56dc6dc
Kay Verbruggen <k.verbruggen@student.tue.nl>
2022-03-18 15:24:42 +0100

    add id's to jobs

--- app/src/main/java/com/example/appdev/Job.java
+++ app/src/main/java/com/example/appdev/Job.java
@@ -1,7 +1,7 @@
 package com.example.appdev;
 
 public class Job {
-    public String title, company, description, imageUrl, street, city, posterId;
+    public String id, title, company, description, imageUrl, street, city, posterId;
     private int mImageResource;
     private boolean isBookmarked;
 
@@ -9,8 +9,9 @@ public class Job {
 
     }
 
-    public Job(String posterId, String title, String company, String description,
+    public Job(String id, String posterId, String title, String company, String description,
                String imageUrl, String street, String city) {
+        this.id = id;
         this.posterId = posterId;
         this.title = title;
         this.company = company;
